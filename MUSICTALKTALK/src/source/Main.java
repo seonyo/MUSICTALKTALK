e4330757ad3b02b43e887bb608b8643254ed5ba6
USER <USER@DESKTOP-F78KM9V>
2023-01-27 21:24:34 +0900

    메인배경화면 이미지 삽입

--- MUSICTALKTALK/src/source/Main.java
+++ MUSICTALKTALK/src/source/Main.java
@@ -1,10 +1,31 @@
 package source;
 
-public class Main {
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	Image background=new ImageIcon(Main.class.getResource("../image/main.png")).getImage();//배경이미지
+	public Main() {
+		homeframe();
+	}
+	public void homeframe() {
+		setTitle("MusicTalkTalk"); //타이틀 명
+		setSize(1200,850); //프레임 사이즈
+		setResizable(false); //프레임 크기 조정 못하게
+		setLocationRelativeTo(null); //창이 가운데로
+		setLayout(null); //레이아웃을 내맘대로 설정 가능
+		setVisible(true); //창이 보이게
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //정상적으로 종료되게
+	}
+	public void paint(Graphics g) {//그리는 함수
+		g.drawImage(background, 0, 0, null);//background를 그려줌
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 new Main();
 	}
 
 }
