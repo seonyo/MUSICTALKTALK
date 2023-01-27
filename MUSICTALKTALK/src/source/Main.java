27163b767afe6bf1f57040a198022b726f3eb64f
USER <USER@DESKTOP-F78KM9V>
2023-01-27 22:02:07 +0900

    메인배경화면 버튼 삽입

--- MUSICTALKTALK/src/source/Main.java
+++ MUSICTALKTALK/src/source/Main.java
@@ -1,16 +1,23 @@
 package source;
 
 
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main extends JFrame{
+	JButton main_b1 = new JButton();
+	JButton main_b2 = new JButton();
 	Image background=new ImageIcon(Main.class.getResource("../image/main.png")).getImage();//배경이미지
+	ImageIcon mainb1_img  = new ImageIcon(Main.class.getResource("../image/main_btn1.png"));//게임설명버튼
+	ImageIcon mainb2_img = new ImageIcon(Main.class.getResource("../image/main_btn2.png")); //게임설명버튼
 	public Main() {
 		homeframe();
+		btn();
 	}
 	public void homeframe() {
 		setTitle("MusicTalkTalk"); //타이틀 명
@@ -20,6 +27,16 @@ public class Main extends JFrame{
 		setLayout(null); //레이아웃을 내맘대로 설정 가능
 		setVisible(true); //창이 보이게
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //정상적으로 종료되게
+	}
+	public void btn() {
+		main_b1.setLayout(null);
+		main_b1.setBounds(800, 500, 290, 50);
+		main_b2.setLayout(null);
+		main_b2.setBounds(800, 600, 290, 50);
+		add(main_b1);
+		add(main_b2);
+		main_b1.setIcon(mainb1_img);
+		main_b2.setIcon(mainb2_img);
 	}
 	public void paint(Graphics g) {//그리는 함수
 		g.drawImage(background, 0, 0, null);//background를 그려줌
