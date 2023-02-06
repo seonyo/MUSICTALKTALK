e2c7d6ee955ee26ad6dd6beb3b4c52acc9ab6105
youngging2 <jsy6535@naver.com>
2023-02-06 20:12:42 +0900

    타이머 구성

--- MUSICTALKTALK/src/source/stage1.java
+++ MUSICTALKTALK/src/source/stage1.java
@@ -4,11 +4,44 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+class TimerBar extends JLabel implements Runnable{
+	int width = 550, height=50;
+	int x=320, y=20;
+	Color color  = new Color(255,0,0);
+	
+	int second;
+	
+	public TimerBar(int second) {
+		setBackground(color);
+		setOpaque(true);
+		setBounds (x,y,width,height);
+		this.second = second;
+	}
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000/ (width/second));
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			if(getWidth()>0) {
+				width-=1;
+				setBounds(x,y,width, height);
+			}else {
+				break;
+			}
+		}
+	}
+}
 
 class stage1 extends JFrame{
 	
@@ -19,8 +52,10 @@ class stage1 extends JFrame{
 		}
 	};
 	
+
+	
 	public stage1() {
-		
+		//stage1 프레임
 		setTitle("MusicTalkTalk"); //타이틀 명
 		setSize(1200,850); //프레임 사이즈
 		setResizable(false); //프레임 크기 조정 못하게
@@ -28,6 +63,7 @@ class stage1 extends JFrame{
 		setLayout(null);
 		int stage_cnt=1;
 		
+		//stage JLabel
 		JLabel cnt = new JLabel();
 		cnt.setBounds(1080,5,50,50);
 		cnt.setText(Integer.toString(stage_cnt));
@@ -35,6 +71,13 @@ class stage1 extends JFrame{
 		cnt.setFont(cnt.getFont().deriveFont(33.0f));
 		add(cnt);
 		
+		int second = 40;
+		TimerBar timerbar = new TimerBar(second);
+		Thread threadBar = new Thread(timerbar);
+		threadBar.start();
+		add(timerbar);
+		
+		//화살표 랜덤
 		int x=350, y=200;
 		Random random = new Random();
 		
@@ -52,7 +95,6 @@ class stage1 extends JFrame{
 				add(left);
 					left.setLayout(null);
 					left.setBounds(x,y,50,50);
-					System.out.println(x);
 					break;
 			
 			case 1 : 
@@ -65,7 +107,6 @@ class stage1 extends JFrame{
 				add(right);
 					right.setLayout(null);
 					right.setBounds(x,y,50,50);
-					System.out.println(x);
 					break;
 					
 			case 2 :
@@ -78,7 +119,6 @@ class stage1 extends JFrame{
 					add(up);
 					up.setLayout(null);
 					up.setBounds(x,y,50,50);
-					System.out.println(x);
 					break;
 					
 			case 3 :
@@ -92,7 +132,6 @@ class stage1 extends JFrame{
 				add(down);
 					down.setLayout(null);
 					down.setBounds(x,y,50,50);
-					System.out.println(x);
 					break;
 					
 			case 4 :
@@ -101,11 +140,10 @@ class stage1 extends JFrame{
 					public void paint(Graphics g) {
 						g.drawImage(space, 0,0,null);
 					}
-				};
+				};				
 				add(space);
 					space.setLayout(null);
 					space.setBounds(x,y,50,50);
-					System.out.println(x);
 					break;
 			}
 			if(i>0 && i<=6) x+=80;
@@ -114,10 +152,11 @@ class stage1 extends JFrame{
 			else if(i<=17) y+=80;
 			else if(i<=25) x+=80;
 		}
+		
+		// 배경 패널 추가
 		panel.setLayout(null);
 		panel.setBounds(0,0,1200,850);
 		add(panel);
-		
 		setVisible(true); //프레임 보이게
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //정상적으로 종료되게
 	}
