4d3246cdcb123f93f0fd99222e1d536fde2a9397
youngging2 <jsy6535@naver.com>
2023-02-18 16:02:35 +0900

    gameover 구현

---
+++ MUSICTALKTALK/src/source/gameover.java
@@ -0,0 +1,40 @@
+package source;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class gameover extends JFrame {
+	JButton End = new JButton();
+	JButton again = new JButton();
+	ImageIcon endimg = new ImageIcon (Main.class.getResource("../image/end.png"));
+	ImageIcon againimg = new ImageIcon (Main.class.getResource("../image/again.png"));
+	
+	Image background = new ImageIcon(Main.class.getResource("../image/gameover.png")).getImage();
+	
+	public void paint (Graphics g) {
+		g.drawImage(background,0,0,null);
+	}
+	public gameover() {
+		setTitle("MusicTalkTalk");
+		setSize(1200,850);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		End.setLayout(null);
+		End.setBounds(200,450,350,50);
+		add(End);
+		End.setIcon(endimg);
+		
+		again.setLayout(null);
+		again.setBounds(650,450,350,50);
+		add(again);
+		again.setIcon(againimg);
+	}
+}
--- MUSICTALKTALK/src/source/stage1.java
+++ MUSICTALKTALK/src/source/stage1.java
@@ -9,6 +9,7 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -17,14 +18,13 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class TimerBar extends JLabel implements Runnable{
-	int width = 550, height=50;
-	int x=320, y=20;
-	Color color  = new Color(255,0,0);
+	int width = 500, height=50; //크기
+	int x=335, y=25; //위치
 	
 	int second;
 	
 	public TimerBar(int second) {
-		setBackground(color);
+		setBackground(Color.yellow);
 		setOpaque(true);
 		setBounds (x,y,width,height);
 		this.second = second;
@@ -37,15 +37,25 @@ class TimerBar extends JLabel implements Runnable{
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
-			if(getWidth()>0) {
-				width-=1;
+			if(getWidth()>250) {
+				width-=3;
 				setBounds(x,y,width, height);
-			}else {
-				break;
 			}
+			else if(getWidth()>=90){
+				width-=3;
+				setBounds(x,y,width, height);
+				setBackground(Color.ORANGE);
+			}
+			else if(getWidth()<90) {
+				width-=3;
+				setBounds(x,y,width, height);
+				setBackground(Color.RED);
+			}
+			else break;
 		}
 	}
 }
+
 
 
 
@@ -59,32 +69,17 @@ class stage1 extends JFrame implements KeyListener{
 			g.drawImage(playpage, 0,0,null);
 		}
 	};
-	
-	JPanel yellow = new JPanel() {
-		Image yellow = new ImageIcon (Main.class.getResource("../image/yellow.png")).getImage();
+	//gameover 이미지 그리기
+	JPanel gameover = new JPanel() {
+		Image gameoverimg = new ImageIcon(Main.class.getResource("../image/gameover.png")).getImage();
 		public void paint (Graphics g) {
-			g.drawImage (yellow, 0,0,null);
+			g.drawImage(gameoverimg, 0,0,null);
 		}
 	};
-	
-	JPanel orange = new JPanel() {
-		Image orange = new ImageIcon (Main.class.getResource("../image/orange.png")).getImage();
-		public void paint (Graphics g) {
-			g.drawImage(orange,0,0,null);
-			}
-	};
-	
-	JPanel blue = new JPanel() {
-		Image blue = new ImageIcon(Main.class.getResource("../image/blue.png")).getImage();
-		public void paint (Graphics g) {
-			g.drawImage(blue,0,0,null);
-		}
-	};
-	
-
 	
 	ArrayList <Integer> arrow_num = new ArrayList<Integer>();
 	ArrayList <JPanel> arrow= new ArrayList <JPanel>();
+	ArrayList <JPanel> heart = new ArrayList <JPanel>();
 	static int score=0;
 	JLabel scoreJL;
 	public stage1() {
@@ -102,9 +97,8 @@ class stage1 extends JFrame implements KeyListener{
 		cnt.setForeground(Color.WHITE);
 		cnt.setFont(cnt.getFont().deriveFont(33.0f));
 		add(cnt);
-	
-		
-		int second = 40;
+
+		int second = 20;
 		TimerBar timerbar = new TimerBar(second);
 		Thread threadBar = new Thread(timerbar);
 		threadBar.start();
@@ -188,21 +182,41 @@ class stage1 extends JFrame implements KeyListener{
 			else if(i<=15) x-=80;
 			else if(i<=17) y+=80;
 			else if(i<=25) x+=80;
-			System.out.println(arrow.get(i-1));
 		}
 		
+		JPanel yellow = new JPanel() {
+			Image yellow = new ImageIcon (Main.class.getResource("../image/yellow.png")).getImage();
+			public void paint (Graphics g) {
+				g.drawImage (yellow, 0,0,null);
+			}
+		};
+		heart.add(yellow);
+
+		JPanel orange = new JPanel() {
+			Image orange = new ImageIcon (Main.class.getResource("../image/orange.png")).getImage();
+			public void paint (Graphics g) {
+				g.drawImage(orange,0,0,null);
+				}
+		};
+		heart.add(orange);
+
+		JPanel blue = new JPanel() {
+			Image blue = new ImageIcon(Main.class.getResource("../image/blue.png")).getImage();
+			public void paint (Graphics g) {
+				g.drawImage(blue,0,0,null);
+			}
+		};
+		
+		heart.add(blue);
+		
 		//아이콘 추가
-		add (yellow);
-		yellow.setLayout(null);
-		yellow.setBounds(260,200,60,50);
 		
-		add (orange);
-		orange.setLayout(null);
-		orange.setBounds(180,200,60,50);
-		
-		add (blue);
-		blue.setLayout(null);
-		blue.setBounds(100,200,60,50);
+		for(int i=0; i<heart.size(); i++) {
+			add(heart.get(i));
+			heart.get(i).setLayout(null);
+			heart.get(i).setBounds(270-(i*80),200,60,60);
+			System.out.println(heart.size());
+		}
 
 		//scoreLabel
 		scoreJL = new JLabel();
@@ -219,8 +233,6 @@ class stage1 extends JFrame implements KeyListener{
 		setVisible(true); //프레임 보이게
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //정상적으로 종료되게
 		addKeyListener(this);
-	
-		
 	}
 	
 	//키보드리스너
@@ -231,10 +243,11 @@ class stage1 extends JFrame implements KeyListener{
 	public void keyReleased(KeyEvent e) {	
 	}
 	
-	int cnt=0, flag;
+	int cnt=0, flag=0, heart_cnt=0, life=3;
 	int y_x=350,y_y=200;
 	int o_x=270,o_y=200;
 	int b_x=190, b_y=200;
+	
 	@Override
 	public void keyPressed (KeyEvent e) {
 		int result;
@@ -243,9 +256,9 @@ class stage1 extends JFrame implements KeyListener{
 				result=0; flag=0;
 				if(result==arrow_num.get(cnt)) {
 					arrow.get(cnt).setSize(0,0);
-					yellow.setLocation(y_x,y_y);
-					orange.setLocation(o_x,o_y);
-					blue.setLocation(b_x, b_y);
+					heart.get(0).setLocation(y_x,y_y);
+					heart.get(1).setLocation(o_x,o_y);
+					heart.get(2).setLocation(b_x, b_y);
 					cnt++; flag=1; score+=50;
 					scoreJL.setText(Integer.toString(score));
 				}
@@ -254,9 +267,9 @@ class stage1 extends JFrame implements KeyListener{
 				result=1; flag=0;
 				if(result==arrow_num.get(cnt)) {
 					arrow.get(cnt).setSize(0,0);
-					yellow.setLocation(y_x,y_y);
-					orange.setLocation(o_x,o_y);
-					blue.setLocation(b_x, b_y);
+					heart.get(0).setLocation(y_x,y_y);
+					heart.get(1).setLocation(o_x,o_y);
+					heart.get(2).setLocation(b_x, b_y);
 					cnt++; flag=1; score+=50;
 					scoreJL.setText(Integer.toString(score));
 
@@ -266,9 +279,9 @@ class stage1 extends JFrame implements KeyListener{
 				result=2; flag=0;
 				if(result==arrow_num.get(cnt)) {
 					arrow.get(cnt).setSize(0,0);
-					yellow.setLocation(y_x,y_y);
-					orange.setLocation(o_x,o_y);
-					blue.setLocation(b_x, b_y);
+					heart.get(0).setLocation(y_x,y_y);
+					heart.get(1).setLocation(o_x,o_y);
+					heart.get(2).setLocation(b_x, b_y);
 					cnt++; flag=1; score+=50;
 					scoreJL.setText(Integer.toString(score));
 
@@ -278,9 +291,9 @@ class stage1 extends JFrame implements KeyListener{
 				result=3; flag=0;
 				if(result==arrow_num.get(cnt)) {
 					arrow.get(cnt).setSize(0,0);
-					yellow.setLocation(y_x,y_y);
-					orange.setLocation(o_x,o_y);
-					blue.setLocation(b_x, b_y);
+					heart.get(0).setLocation(y_x,y_y);
+					heart.get(1).setLocation(o_x,o_y);
+					heart.get(2).setLocation(b_x, b_y);
 					cnt++; flag=1; score+=50;
 					scoreJL.setText(Integer.toString(score));
 
@@ -290,9 +303,9 @@ class stage1 extends JFrame implements KeyListener{
 				result=4; flag=0;
 				if(result==arrow_num.get(cnt)) {
 					arrow.get(cnt).setSize(0,0);
-					yellow.setLocation(y_x,y_y);
-					orange.setLocation(o_x,o_y);
-					blue.setLocation(b_x, b_y);
+					heart.get(0).setLocation(y_x,y_y);
+					heart.get(1).setLocation(o_x,o_y);
+					heart.get(2).setLocation(b_x, b_y);
 					cnt++; flag=1; score+=50;		
 					scoreJL.setText(Integer.toString(score));
 				}
@@ -318,6 +331,17 @@ class stage1 extends JFrame implements KeyListener{
 				else if(cnt<=17) b_x-=80;
 				else if(cnt<=19) b_y+=80;
 				else if(cnt<=25) b_x+=80;
+			}
+			else {
+				heart.get(0).setSize(0,0);
+				heart_cnt++;
+				Collections.swap(heart, 0, 1);
+				Collections.swap(heart, 1,2);
+				--life;
+				
+				if(life==0) {
+					new gameover();
+				}
 			}
 	}
 }
